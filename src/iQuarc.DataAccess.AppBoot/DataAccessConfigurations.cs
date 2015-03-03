8385873d2190a9ef4845edff9b1431d2495d80b7
Florin Coros <florin.coros@iquarc.com>
2015-03-03 11:10:24 +0200

    add configuration for AppBoot into DataAccess,AppBoot

---
+++ src/iQuarc.DataAccess.AppBoot/DataAccessConfigurations.cs
@@ -0,0 +1,22 @@
+﻿using iQuarc.AppBoot;
+
+namespace iQuarc.DataAccess.AppBoot
+{
+	public static class DataAccessConfigurations
+	{
+		public static ConventionRegistrationBehavior DefaultRegistrationConventions
+		{
+			get { return GetDefaultConventions(); }
+		}
+
+		private static ConventionRegistrationBehavior GetDefaultConventions()
+		{
+			var conventions = new ConventionRegistrationBehavior();
+
+			conventions.ForType<Repository>().Export(b => b.AsContractType<IRepository>());
+			conventions.ForType<InterceptorsResolver>().Export(b => b.AsContractType<IInterceptorsResolver>().WithLifetime(Lifetime.Application));
+
+			return conventions;
+		}
+	}
+}
